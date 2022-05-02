c2d54e93a4847c3f246ef2e726d896ffcb46b1c4
dimitriizz <dimitrivw00@gmail.com>
2022-05-02 10:09:40 +0300

    ok

---
+++ hangman/#hangman.py
@@ -0,0 +1,30 @@
+#hangman
+
+#imports
+
+import random
+
+
+sanat = [
+'omena',
+'lohi',
+'kana',
+'soija',
+'tofu',
+'kahvi',
+'päärynä',
+'banaani',
+'riisi',
+'peruna',
+'sorsa',
+'vittu',
+'saatana',
+'helvetti'
+]
+
+def sana_rand():
+    sana = random.choice(sanat)
+    return word.upper()
+
+
+
